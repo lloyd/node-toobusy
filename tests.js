1cc5e93dc02588196a7f21c8194162b0ef806371
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-11-17 15:35:24 -0700

    tests and travis file

---
+++ tests.js
@@ -0,0 +1,43 @@
+const
+should = require('should'),
+toobusy = require('./');
+
+describe('the library', function() {
+  it('should export a couple functions', function(done) {
+    (toobusy).should.be.a('function');
+    (toobusy.maxLag).should.be.a('function');
+    (toobusy.shutdown).should.be.a('function');
+    done();
+  });
+});
+
+describe('maxLag', function() {
+  it('should default to 70', function(done) {
+    (toobusy.maxLag()).should.equal(70);
+    done();
+  });
+  it('should throw an exception for values < 10', function(done) {
+    (function() { toobusy.maxLag(9); }).should.throw;
+    done();
+  });
+  it('should be configurable', function(done) {
+    (toobusy.maxLag(50)).should.equal(50);
+    (toobusy.maxLag(10)).should.equal(10);
+    (toobusy.maxLag()).should.equal(10);
+    done();
+  });
+});
+
+describe('toobusy()', function() {
+  it('should return true after a little load', function(done) {
+    function load() {
+      if (toobusy()) return done();
+      var start = new Date();
+      while ((new Date() - start) < 250) {
+        for (var i = 0; i < 1e5;) i++;
+      }
+      setTimeout(load, 0);
+    }
+    load();
+  });
+});
